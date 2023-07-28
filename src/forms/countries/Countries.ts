2efbb3c5760db356c779b69e05e9e905e22f612d
ahoeffner <alex@hoeffner.net>
2023-07-29 00:38:04 +0200

    Update Countries.ts

--- src/forms/countries/Countries.ts
+++ src/forms/countries/Countries.ts
@@ -1,6 +1,11 @@
-import { Form } from "forms42core";
+import { EventType, Form, FormEvent, formevent } from "forms42core";
 
 export class Countries extends Form
 {
-    
+   @formevent()
+   public async test(event:FormEvent) : Promise<boolean>
+   {
+      console.log(EventType[event.type]);
+      return(true);
+   }
 }
