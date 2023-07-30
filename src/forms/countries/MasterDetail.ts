6f6357d6a2d5ad59d93a2f43119cdfdc15909e07
ahoeffner <alex@hoeffner.net>
2023-07-30 13:40:34 +0200

    .

--- src/forms/countries/MasterDetail.ts
+++ src/forms/countries/MasterDetail.ts
@@ -1,7 +1,28 @@
+/*
+  MIT License
+
+  Copyright © 2023 Alex Høffner
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+  and associated documentation files (the “Software”), to deal in the Software without
+  restriction, including without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or
+  substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+import { Lov } from "./Lov";
 import { Employees } from "../../datasources/Employees";
 import { Departments } from "../../datasources/Departments";
-import { Block, EventType, Form, Key, datasource, formevent } from "forms42core";
-import { Lov } from "./Lov";
+import { Block, Form, Key, datasource } from "forms42core";
 
 
 @datasource("employees",Employees)
@@ -30,7 +51,7 @@ export class MasterDetail extends Form
 			blk.datasource.sorting = column;
 			await blk.reQuery();
 		}
-		
+
 		return(true);
 	}
 }
